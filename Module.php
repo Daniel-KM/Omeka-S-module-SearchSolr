fddde9f11f9aadcc2a4c49451a1fd7d28f80355c
Julian Maurice <julian.maurice@biblibre.com>
2016-05-18 08:25:54 +0200

    Allow anonymous users to use the search

--- Module.php
+++ Module.php
@@ -29,6 +29,7 @@
 
 namespace Solr;
 
+use Zend\Mvc\MvcEvent;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Omeka\Module\AbstractModule;
 use Omeka\Module\Exception\ModuleCannotInstallException;
@@ -38,6 +39,14 @@ class Module extends AbstractModule
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $event)
+    {
+        parent::onBootstrap($event);
+
+        $acl = $this->getServiceLocator()->get('Omeka\Acl');
+        $acl->allow(null, 'Solr\Api\Adapter\SolrFieldAdapter');
     }
 
     public function install(ServiceLocatorInterface $serviceLocator)
