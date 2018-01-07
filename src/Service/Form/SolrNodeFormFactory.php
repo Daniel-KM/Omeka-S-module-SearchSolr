4208443a8317cdf904afdc6ec0b93ebc73b16fc4
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-01-08 00:00:00 +0100

    Fixed config form.

--- src/Service/Form/SolrNodeFormFactory.php
+++ src/Service/Form/SolrNodeFormFactory.php
@@ -9,7 +9,7 @@ class SolrNodeFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new SolrNodeForm;
+        $form = new SolrNodeForm(null, $options);
         $form->setTranslator($services->get('MvcTranslator'));
 
         return $form;
