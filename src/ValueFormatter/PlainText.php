f06e2b883d6c847234809a0d6d93f549811127b0
Paul Sarrassat <poltron54@gmail.com>
2018-02-26 19:01:54 +0100

    Add PlainText value formatter to strip HTML tags

---
+++ src/ValueFormatter/PlainText.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Solr\ValueFormatter;
+
+/**
+ * ValueFormatter to strip all HTML tags. Useful for HTML media content.
+ */
+class PlainText implements ValueFormatterInterface
+{
+    public function getLabel() {
+        return 'Plain text';
+    }
+
+    public function format($value) {
+        return strip_tags($value);
+    }
+}
