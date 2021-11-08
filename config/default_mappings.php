b45650b0d00675f2285b683a567f87011a52e002
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-11-08 00:00:00 +0000

    Updated default core config.

--- config/default_mappings.php
+++ config/default_mappings.php
@@ -14,19 +14,45 @@ return [
 
     // Required specific fields for any type of resource.
     [
-        'resource_name' => 'resources',
+        'resource_name' => 'generic',
         'field_name' => 'resource_name_s',
         'source' => 'resource_name',
         'pool' => [],
-        'settings' => [],
+        'settings' => ['formatter' => '', 'label' => 'Resource type'],
     ],
     [
-        'resource_name' => 'resources',
+        'resource_name' => 'generic',
         'field_name' => 'is_public_b',
         'source' => 'is_public',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Public'],
     ],
+    [
+        'resource_name' => 'generic',
+        'field_name' => 'owner_id_i',
+        'source' => 'owner/o:id',
+        'pool' => [],
+        'settings' => [],
+    ],
+    [
+        'resource_name' => 'generic',
+        'field_name' => 'site_id_is',
+        'source' => 'site/o:id',
+        'pool' => [],
+        'settings' => [],
+    ],
+
+    // Not required specific fields.
+    /* For shared core with Drupal.
+    [
+        'resource_name' => 'generic',
+        'field_name' => 'index_id',
+        'source' => 'search_index',
+        'pool' => [],
+        'settings' => [],
+    ],
+    */
+
     [
         'resource_name' => 'resources',
         'field_name' => 'resource_class_id_i',
@@ -41,75 +67,41 @@ return [
         'pool' => [],
         'settings' => [],
     ],
-    [
-        'resource_name' => 'resources',
-        'field_name' => 'owner_id_i',
-        'source' => 'owner/o:id',
-        'pool' => [],
-        'settings' => [],
-    ],
-    [
-        'resource_name' => 'resources',
-        'field_name' => 'site_id_is',
-        'source' => 'site/o:id',
-        'pool' => [],
-        'settings' => [],
-    ],
 
-    // Not required specific fields.
-    /* For shared core with Drupal.
-    [
-        'resource_name' => 'resources',
-        'field_name' => 'index_id',
-        'source' => 'search_index',
-        'pool' => [],
-        'settings' => [],
-    ],
-    */
-
-    // Items.
-
-    // Required fields.
-    [
-        'resource_name' => 'items',
-        'field_name' => 'item_set_id_is',
-        'source' => 'item_set/o:id',
-        'pool' => [],
-        'settings' => ['formatter' => '', 'label' => 'Item set id'],
-    ],
+    // Resources.
 
     // Properties.
     // Text general of Dublin Core elements + spatial and temporal coverages.
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_title_txt',
         'source' => 'dcterms:title',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Title'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_creator_txt',
         'source' => 'dcterms:creator',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Creator'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_subject_txt',
         'source' => 'dcterms:subject',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Subject'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_description_txt',
         'source' => 'dcterms:description',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Description'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_publisher_txt',
         'source' => 'dcterms:publisher',
         'pool' => [],
@@ -123,77 +115,77 @@ return [
         'settings' => ['formatter' => '', 'label' => 'Contributor'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_date_txt',
         'source' => 'dcterms:date',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Date'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_type_txt',
         'source' => 'dcterms:type',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Type'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_format_txt',
         'source' => 'dcterms:format',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Format'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_identifier_txt',
         'source' => 'dcterms:identifier',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Identifier'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_source_txt',
         'source' => 'dcterms:source',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Source'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_language_txt',
         'source' => 'dcterms:language',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Language'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_relation_txt',
         'source' => 'dcterms:relation',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Relation'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_coverage_txt',
         'source' => 'dcterms:coverage',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Coverage'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_rights_txt',
         'source' => 'dcterms:rights',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Rights'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_spatial_txt',
         'source' => 'dcterms:spatial',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Spatial coverage'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_temporal_txt',
         'source' => 'dcterms:temporal',
         'pool' => [],
@@ -201,14 +193,14 @@ return [
     ],
     // Dublin Core Terms.
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_spatial_ss',
         'source' => 'dcterms:spatial',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Spatial coverage'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_temporal_dr',
         'source' => 'dcterms:temporal',
         'pool' => [],
@@ -217,49 +209,49 @@ return [
 
     // Fields for facets.
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_type_ss',
         'source' => 'dcterms:type',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Type'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_subject_ss',
         'source' => 'dcterms:subject',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Subject'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_creator_ss',
         'source' => 'dcterms:creator',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Creator'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_publisher_ss',
         'source' => 'dcterms:publisher',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Publisher'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_language_ss',
         'source' => 'dcterms:language',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Language'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_rights_ss',
         'source' => 'dcterms:rights',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Rights'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'item_set_dcterms_title_ss',
         'source' => 'item_set/dcterms:title',
         'pool' => [],
@@ -268,14 +260,14 @@ return [
 
     // Specific fields.
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'resource_class_s',
         'source' => 'resource_class/o:local_name',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Resource class'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'resource_template_s',
         'source' => 'resource_template/o:label',
         'pool' => [],
@@ -284,50 +276,38 @@ return [
 
     // Fields to sort.
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_title_s',
         'source' => 'dcterms:title',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Title'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_date_s',
         'source' => 'dcterms:date',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Date'],
     ],
     [
-        'resource_name' => 'items',
+        'resource_name' => 'resources',
         'field_name' => 'dcterms_creator_s',
         'source' => 'dcterms:creator',
         'pool' => [],
         'settings' => ['formatter' => '', 'label' => 'Creator'],
     ],
 
-    // Item sets.
+    // Items.
 
-    // Dublin core.
+    // Required fields.
     [
-        'resource_name' => 'item_sets',
-        'field_name' => 'dcterms_title_txt',
-        'source' => 'dcterms:title',
+        'resource_name' => 'items',
+        'field_name' => 'item_set_id_is',
+        'source' => 'item_set/o:id',
         'pool' => [],
-        'settings' => ['formatter' => '', 'label' => 'Title'],
+        'settings' => ['formatter' => '', 'label' => 'Item set id'],
     ],
-    [
-        'resource_name' => 'item_sets',
-        'field_name' => 'dcterms_description_txt',
-        'source' => 'dcterms:description',
-        'pool' => [],
-        'settings' => ['formatter' => '', 'label' => 'Description'],
-    ],
-    // Fields to sort.
-    [
-        'resource_name' => 'item_sets',
-        'field_name' => 'dcterms_title_s',
-        'source' => 'dcterms:title',
-        'pool' => [],
-        'settings' => ['formatter' => '', 'label' => 'Title'],
-    ],
+
+    // Item sets.
+    // Nothing specific.
 ];
