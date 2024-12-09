80a496f3c30bb493cc3c58ccb0415d96504f0725
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-12-09 00:00:00 +0000

    Allowed to have offset hour and minutes without a colon.

--- src/ValueFormatter/Date.php
+++ src/ValueFormatter/Date.php
@@ -61,7 +61,7 @@ class Date extends AbstractValueFormatter
     {
         $yearMin = -292277022656;
         $yearMax = 292277026595;
-        $patternIso8601 = '^(?<date>(?<year>-?\d{4,})(-(?<month>\d{1,2}))?(-(?<day>\d{1,2}))?)(?<time>(T(?<hour>\d{1,2}))?(:(?<minute>\d{1,2}))?(:(?<second>\d{1,2}))?)(?<offset>((?<offset_hour>[+-]\d{1,2})?(:(?<offset_minute>\d{1,2}))?)|Z?)$';
+        $patternIso8601 = '^(?<date>(?<year>-?\d{4,})(-(?<month>\d{1,2}))?(-(?<day>\d{1,2}))?)(?<time>(T(?<hour>\d{1,2}))?(:(?<minute>\d{1,2}))?(:(?<second>\d{1,2}))?)(?<offset>((?<offset_hour>[+-]\d{1,2})?(:?(?<offset_minute>\d{1,2}))?)|Z?)$';
         static $dateTimes = [];
 
         $value = ltrim($value, '+');
