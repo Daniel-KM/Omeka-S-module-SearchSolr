biblibre/omeka-s-module-Solr

Deny access to solr_* resources through the API to non-admin users
Closes #42

src/Service/Controller/ApiControllerFactory.php
@@ -1,0 +1,17 @@
+<?php
+namespace Solr\Service\Controller;
+
+use Interop\Container\ContainerInterface;
+use Solr\Controller\ApiController;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+
+class ApiControllerFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        return new ApiController(
+            $services->get('Omeka\Paginator'),
+            $services->get('Omeka\ApiManager')
+        );
+    }
+}

src/Service/Controller/ApiLocalControllerFactory.php
@@ -1,0 +1,17 @@
+<?php
+namespace Solr\Service\Controller;
+
+use Interop\Container\ContainerInterface;
+use Solr\Controller\ApiLocalController;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+
+class ApiLocalControllerFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        return new ApiLocalController(
+            $services->get('Omeka\Paginator'),
+            $services->get('Omeka\ApiManager')
+        );
+    }
+}

Daniel-KM/Omeka-S-module-SearchSolr

src/Service/Controller/ApiControllerFactory.php
@@ -1,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace SearchSolr\Service\Controller;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use SearchSolr\Controller\ApiController;
+
+class ApiControllerFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        return new ApiController(
+            $services->get('Omeka\Paginator'),
+            $services->get('Omeka\ApiManager')
+        );
+    }
+}

src/Service/Controller/ApiLocalControllerFactory.php
@@ -1,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace SearchSolr\Service\Controller;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use SearchSolr\Controller\ApiLocalController;
+
+class ApiLocalControllerFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        return new ApiLocalController(
+            $services->get('Omeka\Paginator'),
+            $services->get('Omeka\ApiManager')
+        );
+    }
+}